3e763454068bd863ca4defd8b71d67f3e02f1e59
samxaradzenika <murdoc746@gmail.com>
2023-07-02 20:37:23 +0400

    sdsadsa

--- scripts/setup.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const db = require("@arangodb").db;
-const collectionName = "sessionDetails";
-
-if (!db._collection(collectionName)) {
-  db._createDocumentCollection(collectionName);
-}
